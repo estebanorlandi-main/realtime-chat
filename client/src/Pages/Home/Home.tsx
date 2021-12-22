bb4157c6752cf535677c0a78929d291fae3a5ffe
estebanorlandi4 <estebanorlandi4@gmail.com>
2021-12-22 16:19:16 -0300

    feat: realtime update

--- client/src/Pages/Home/Home.tsx
+++ client/src/Pages/Home/Home.tsx
@@ -1,11 +1,38 @@
 import ShowChats from "../../Components/ShowChats/ShowChats";
 import { Navigate, Outlet } from "react-router";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { State } from "../../Redux/reducers";
 import { IUser } from "../../Utils/interfaces";
+import { actionCreators } from "../../Redux";
+import { bindActionCreators } from "redux";
+import { useEffect, useState } from "react";
 
 function Home() {
+  const dispatch = useDispatch();
   const session: IUser = useSelector((state: State) => state.session);
+  const io: any = useSelector((state: State) => state.socket);
+  const [newMessage, setNewMessage] = useState(false);
+
+  const { getChats, connectSocket, disconnectSocket } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
+
+  useEffect(() => {
+    connectSocket();
+    return () => {
+      disconnectSocket();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (newMessage) getChats(session.username);
+    setNewMessage(false);
+  }, [newMessage]);
+
+  io?.on("new_message", (data: any) => {
+    setNewMessage(true);
+  });
 
   return session.username ? (
     <div className="home">
